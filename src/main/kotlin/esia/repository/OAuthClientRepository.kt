8a78d6a04a2970b5336f33664c9ea8825d86d4f5
Mihail Butvin <butvin.mihail@yandex.ru>
2025-05-30 05:19:23 +0300

    Replace custom service with Keycloak

--- src/main/kotlin/esia/repository/OAuthClientRepository.kt
+++
@@ -1,7 +0,0 @@
-package esia.repository
-
-import esia.model.OAuthClient
-import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.stereotype.Repository
-
-@Repository interface OAuthClientRepository : JpaRepository<OAuthClient, String>
